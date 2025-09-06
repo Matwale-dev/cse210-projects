00d96733f26f2b3f58eb1e4e2a13d02fcc329cff
matwale-dev <matwale22@yahoo.co.uk>
2025-09-06 19:40:53 +0300

    Guessed it

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        var rnd = new Random();
+        int magicNumber = rnd.Next(1, 101); // random number in [1,100]
+        int guess = -1;
+        int attempts = 0;
+
+        Console.WriteLine("I'm thinking of a number between 1 and 100.");
+        // Uncomment the next line while testing to reveal the secret:
+        // Console.WriteLine($"(Debug) Magic number is: {magicNumber}");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out guess))
+            {
+                Console.WriteLine("Please enter a valid integer.");
+                continue;
+            }
+
+            attempts++;
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else // guess == magicNumber
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {attempts} attempt{(attempts == 1 ? "" : "s")}.");
+            }
+        }
     }
 }
